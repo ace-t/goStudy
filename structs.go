bfd9db62aa22a3ad07529a5139361895bb7ce364
Terry.Park <terry.park@daumkakao.com>
2017-02-03 18:42:06 +0900

    구조체 관련

---
+++ structs.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+}
+
+func main() {
+	fmt.Println(person{"AceT", 17})
+	fmt.Println(person{name: "AceT2", age: 27})
+	fmt.Println(person{name: "Mae"})
+	fmt.Println(&person{name: "Sun", age: 40})
+
+	s := person{name: "Moon", age: 50}
+	fmt.Println(s.name)
+
+	/*  result :
+	{AceT 17}
+	{AceT2 27}
+	{Mae 0}
+	&{Sun 40}
+	Moon
+	*/
+
+	sp := &s
+	fmt.Println(sp.age) // 50
+
+	sp.age = 100
+	fmt.Println(sp.age) // 100
+
+}
